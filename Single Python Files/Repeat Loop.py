f274612955c11dd8cb2a8bb81983adb93112daa2
Wrath Lord <72062208+WrathLord935@users.noreply.github.com>
2023-08-07 20:37:34 +0530

    A simple Repeat Loop for copy-pasting into other code

---
+++ Single Python Files/Repeat Loop.py
@@ -0,0 +1,9 @@
+#Repeat Loop
+
+h = 'Y'
+while h == 'Y' or h == 'y':
+    h = input('Run Again?(Y/N)\n')
+
+while h == 'N' or h == 'n':
+    print('Thank you and Goodbye\n\tCreated by Wrath Lord')
+    break
